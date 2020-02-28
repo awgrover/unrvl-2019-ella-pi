036490d850c35c585b698a1518c61a66cdb667e7
Alan Grover <awgrover@gmail.com>
2020-02-28 11:59:46 -0500

    init

---
+++ ADCTouchAndMessage/ADCTouchAndMessage.ino
@@ -0,0 +1,102 @@
+/*
+  Test for A0..A5, plot them, with a "detect touch"
+  
+  On the "tiny"< only A0..A3=int 0...3 = digital 5,2,4,3
+
+  Very clean +-5, prop response.
+  90/4 =~ 22ms each
+  134/5 =~ 26ms each
+
+  Test setup
+    3 actual wires
+      foll covered pill bottle
+      stainless press pot
+      alum beer can
+      ~620 "base" (prop to length)
+      ~900 touch
+      ~700 w/1 layer tape
+      about 2 inch range to start detect
+    No wire: ~400, almost insensitive
+
+*/
+#include <ADCTouch.h>
+
+template <typename T, unsigned S> inline unsigned arraysize(const T (&v)[S]) {
+  return S;
+};
+
+struct Touch {
+  const int pin; // an A0.. up to A3
+  int ref;  // do x.read(500);a x.ref=x.value;  to get a reference base
+  int value;
+
+  void read(int tries = 100) {
+    // sets .value
+    this->value = ADCTouch.read(pin, tries);
+  }
+  boolean touched(float of_remaining = 0.25) {
+    // empirically, a touch is about 1/4 of the remaining "head space" to 1024
+    return (1024 - ref) * of_remaining < (value - ref);
+  }
+};
+Touch Touches[] = {{A0}, {A1}, {A2}, {A3}, {A4}, {A5}};
+
+void setup()
+{
+  // No pins to setup, pins can still be used regularly, although it will affect readings
+
+  Serial.begin(115200);
+  Serial.println("Start");
+
+  // header
+  Serial.print("time"); Serial.print(": ");
+  for (Touch &atouch : Touches) {
+    Serial.print("pin"); Serial.print(atouch.pin); Serial.print(": ");
+    Serial.print("value"); Serial.print(": ");
+    Serial.print("+touch"); Serial.print(": ");
+  }
+    Serial.println();
+
+  for (Touch &atouch : Touches) {
+    atouch.read(500);
+    atouch.ref = atouch.value;
+    //Serial.print((long) &atouch); Serial.print(" ");
+    Serial.print(atouch.pin); Serial.print(" ");
+    Serial.print(atouch.ref); Serial.print(" ");
+    Serial.println();
+  }
+  Touch &atouch = Touches[1];
+  /*
+    Serial.print("x "); Serial.print(atouch.pin); Serial.print(" ");
+    Serial.print(atouch.ref); Serial.print(" ");
+    Serial.print(atouch.value); Serial.print(" ");
+    Serial.println();
+  */
+}
+
+void loop()
+{
+  unsigned long start = micros();
+
+  for (Touch &atouch : Touches) {
+    atouch.read(100);
+  }
+
+  unsigned long delta = micros() - start;
+  Serial.print(delta / 100); Serial.print(" ");
+
+  //value0 -= ref0;       //remove offset
+  //value1 -= ref1;
+
+  for (Touch &atouch : Touches) {
+    //Serial.print((long) &atouch); Serial.print("] ");
+    Serial.print(atouch.ref); Serial.print(" ");
+    Serial.print(atouch.value); Serial.print(" ");
+    Serial.print(atouch.touched() * 20 + atouch.ref); Serial.print(" ");
+    //break;
+  }
+
+
+  Serial.println();
+  //delay(100);
+}
